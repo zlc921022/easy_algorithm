b19ec950655fce940120bdd63eaf2400efd3ae37
zlc921022 <zlc921022@163.com>
2020-05-08 23:52:27 +0800

    添加题目

---
+++ src/leetcode/editor/cn/T83.java
@@ -0,0 +1,43 @@
+package leetcode.editor.cn;
+
+//[83]删除排序链表中的重复元素
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表
+
+
+public class T83 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
