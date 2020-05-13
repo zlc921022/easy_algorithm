735e9b877b13351d5264e83c4ba2069de4ea3e1d
zlc <zlc921022@163.com>
2020-05-13 10:23:48 +0800

    重复题目记录提交

---
+++ src/leetcode/editor/cn/T24.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+//[面试题24]反转链表
+//定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。 
+//
+// 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 5000 
+//
+// 
+//
+// 注意：本题与主站 206 题相同：https://leetcode-cn.com/problems/reverse-linked-list/ 
+// Related Topics 链表
+
+
+import org.junit.Test;
+
+public class T24 {
+
+    public static void main(String[] args) {
+
+    }
+    
+    @Test
+    public void test(){
+    
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /**
+     * 同T206题目
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
