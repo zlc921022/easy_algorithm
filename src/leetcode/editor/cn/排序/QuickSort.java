6ebcf1eeea182697bbdd368f4d9bb996b6f08f7c
zlc <zlc921022@163.com>
2020-05-27 18:21:18 +0800

    快排

--- src/leetcode/editor/cn/排序/QuickSort.java
+++ src/leetcode/editor/cn/排序/QuickSort.java
@@ -1,6 +1,9 @@
 package leetcode.editor.cn.排序;
 
+import com.sun.deploy.util.ArrayUtil;
 import org.junit.Test;
+
+import java.util.Random;
 
 /**
  * @author zlc
@@ -12,13 +15,19 @@ public class QuickSort {
     @Test
     public void test() {
 
-        int[] array = {9,8,7,6,5,4,3,2,1};
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array.length - 1- i;
+        }
         System.out.println("排序前： ");
         for (int value : array) {
             System.out.print(value + " ");
         }
+        long startTime = System.currentTimeMillis();
         System.out.println();
         quickSort(array, 0, array.length - 1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("运行时间：" + (endTime - startTime));
         System.out.println("排序后： ");
         for (int value : array) {
             System.out.print(value + " ");
@@ -30,34 +39,50 @@ public class QuickSort {
         if (start >= end) {
             return;
         }
-        int partition = partition1(arr, start, arr.length - 1);
+        int partition = partition(arr, start, end);
         quickSort(arr, start, partition - 1);
         quickSort(arr, partition + 1, end);
     }
 
-    private int partition(int[] arr, int start, int end) {
-        int pivot = arr[start];
-        int l = start;
-        int r = end;
-        while (l < r) {
-            while (l < r && arr[l] < pivot) {
-                l++;
-            }
-            while (l < r && arr[r] > pivot) {
-                r--;
-            }
-            if (l >= r) {
-                break;
-            }
-            if (arr[l] != arr[r]) {
-                int temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
-            } else {
-                l++;
+    private int partition0(int[] arr, int left, int right){
+        int ran = (int) (Math.random() * (right - left + 1) + left);
+        swap(arr,left,ran);
+        int pivot = arr[left];
+        int j = left + 1;
+        for (int i = j; i <= right; i++) {
+            if(arr[i] < pivot){
+                swap(arr,i,j++);
             }
         }
-        return l;
+        swap(arr,left,j - 1);
+        return j - 1;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int ran = (int) (Math.random() * (right - left + 1) + left);
+        swap(arr,left,ran);
+        int pivot = arr[left];
+        int i = left;
+        int j = right;
+        while (i < j) {
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            while (i < j && arr[i] <= pivot) {
+                i++;
+            }
+            if(i < j){
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,left,j);
+        return j;
+    }
+
+    private void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     private int partition1(int[] arr, int start, int end) {
